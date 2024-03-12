2565f9f5b7751ed58b8040872f71323b320f9bcb
nabcellent <nabcellent.dev@gmail.com>
2024-03-12 18:24:03 +0300

    Adds responsive design

--- src/layouts/GuestLayout.tsx
+++ src/layouts/GuestLayout.tsx
@@ -5,29 +5,29 @@ import { CONFIG } from '@/config.ts';
 import { IMAGES } from '@/constants/images';
 
 const GuestLayout = () => (
-    <div className={'flex items-center min-h-screen p-6'}>
-        <div className="mx-auto">
-            <div className={'flex items-center justify-center fs-5 mb-4 flex-col relative'}>
-                <img
-                    className="absolute end-[-8.75rem] top-[-5.125rem]"
-                    src={IMAGES.icons.spotIllustrations.bg_shape}
-                    width="250"
-                />
-                <img
-                    className="absolute start-[-6.75rem] bottom-[-2.4375rem]"
-                    src={IMAGES.icons.spotIllustrations.shape_1}
-                    width="150"
-                />
+    <div className={'flex flex-col relative items-center justify-center min-h-screen min-w-screen p-6'}>
+        <div className={'flex items-center justify-center mb-4 flex-col relative'}>
+            <img
+                alt={''}
+                className="absolute end-[-8.75rem] top-[-5.125rem]"
+                src={IMAGES.icons.spotIllustrations.bg_shape}
+                width="250"
+            />
+            <img
+                alt={''}
+                className="absolute start-[-6.75rem] bottom-[-2.4375rem]"
+                src={IMAGES.icons.spotIllustrations.shape_1}
+                width="150"
+            />
 
-                <img className="me-2" src={IMAGES.logos.sidooh} alt="Logo" width={120} />
-                <h6 className={'font-semibold text-[10pt]'} style={{ margin: `-.48rem 0 2rem` }}>
-                    {CONFIG.app.name}
-                </h6>
+            <img className="me-2" src={IMAGES.logos.sidooh} alt="Logo" width={120} />
+            <h6 className={'font-semibold text-[10pt]'} style={{ margin: `-.48rem 0 2rem` }}>
+                {CONFIG.app.name}
+            </h6>
 
-                <Suspense fallback={<PageLoader />}>
-                    <Outlet />
-                </Suspense>
-            </div>
+            <Suspense fallback={<PageLoader />}>
+                <Outlet />
+            </Suspense>
         </div>
     </div>
 );
