71eebbc6f1b676eb72b8af9b77d3208d40d39e79
nabcellent <nabcellent.dev@gmail.com>
2024-03-18 17:52:28 +0300

    Aligns transaction confirmation content to left

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -1,26 +1,16 @@
 import { CONFIG } from '@/config.ts';
 
-const Footer = () => {
-    const today = new Date();
-
-    const day = today.toLocaleDateString('en-US', { weekday: 'long' });
-    const date = today.getDate();
-    const month = today.toLocaleDateString('en-US', { month: 'long' });
-
-    return (
-        <footer className="flex justify-between text-xs pb-3 container">
-            <p className="text-600 flex gap-1">
-                {CONFIG.app.name} |
-                <a href="/public" className={'text-primary'}>
-                    Sidooh
-                </a>
-                &copy; {new Date().getFullYear()}
-            </p>
-            <p className="text-600">
-                v{CONFIG.app.version} | {day}, {date} {month}
-            </p>
-        </footer>
-    );
-};
+const Footer = () => (
+    <footer className="flex justify-between text-xs pb-3 container">
+        <p className="text-600 flex gap-1">
+            {CONFIG.app.name} |
+            <a href="/public" className={'text-primary'}>
+                Sidooh
+            </a>
+            &copy; {new Date().getFullYear()}
+        </p>
+        <p className="text-600">v{CONFIG.app.version}</p>
+    </footer>
+);
 
 export default Footer;
